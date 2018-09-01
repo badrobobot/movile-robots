d24c8301d0548e912b8407920b945101b146812b
Elias David Lara Guzmán <42329259+badrobobot@users.noreply.github.com>
2018-09-01 18:03:20 -0500

    Update prac02_sensors.ino

--- prac02_sensors.ino
+++ prac02_sensors.ino
@@ -3,7 +3,8 @@
   Practice 2 
   Bio-Robotics 
   Fi-UNAM
-
+  ASK THEN READ AND PRINT
+  
 */
 
 #define pin_contact   2
@@ -11,6 +12,8 @@
 #define pin_photora   A1 
 #define pin_photord   A2
 #define pin_infrared  A3
+const int a = 50;
+const int b = 50;
 
 String inputString = "";         // a String to hold incoming data
 boolean stringComplete = false;  // whether the string is complete
@@ -19,6 +22,7 @@ boolean stringComplete = false;  // whether the string is complete
 void setup() {
   // put your setup code here, to run once:
   pinMode(pin_contact,INPUT);
+  pinMode(pin_photord,INPUT);
   Serial.begin(9600);
   // reserve 200 bytes for the inputString:
   inputString.reserve(200);
@@ -43,16 +47,20 @@ void loop() {
       Serial.print("photord ");
       photord();
     }
-    if(inputString=="shs temp"){
+    if(inputString == "shs temp"){
       Serial.print("temp ");
       temp();
     }
-    if(inputString=="shs infrared"){
+    if(inputString == "shs infrared"){
       Serial.print("infrared ");
       infrared();
     }
+    if(inputString.equals("a")){
+      Serial.print("OK");
+      infrared();
+    }
     else{
-      Serial.print("Error: No sensor ");
+      Serial.print("Error no sensor:");
       Serial.print(inputString);
     }
     
@@ -86,14 +94,14 @@ void contact() {
 
 void temp(){
   float temperatura = 0;
-  temperatura = digitalRead(pin_temp);
+  temperatura = 5000*10*digitalRead(pin_temp)/1024;
   Serial.println(temperatura);  
   delay(1); 
 }
 
 void photora(){
   float photora = 0;
-  photora = digitalRead(pin_photora);
+  photora = analogRead(pin_photora);
   Serial.println(photora);
   delay(1);   
 }
@@ -107,7 +115,19 @@ void photord(){
 
 void infrared(){
   float infrared = 0;
-  infrared = digitalRead(pin_infrared);
+  infrared = a*pow(analogRead(pin_infrared),b);
   Serial.println(infrared);
   delay(1);   
+}
+
+float calibrate_infrarred(int n)
+{
+  long suma=0;
+  for(int i=0;i<n;i++)
+  {
+    suma=suma+analogRead(A0);
+  }  
+  float adc=suma/n;
+  float distancia_cm = 17569.7 * pow(adc, -1.2062);
+  return(distancia_cm);
 }
